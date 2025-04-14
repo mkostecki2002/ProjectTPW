64b34a1efc0b13ab16dd4ecf67a47042afd59430
252936 <nomysz0987654321@op.pl>
2025-04-14 20:34:46 +0200

    Testy zrobione

--- Model/BallModel.cs
+++ Model/BallModel.cs
@@ -35,7 +35,6 @@ namespace Model
             {
                 Ball ballToRemove = Balls.Last();
                 Balls.Remove(ballToRemove);
-                // Optionally, stop the thread for the removed ball
             }
         }
     }
